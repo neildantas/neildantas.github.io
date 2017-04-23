75c5c0362f6afa6fc2b778090e319e21624e7f41
Anubha <anubha007@gmail.com>
2017-04-24 01:36:06 +0530

    first commit

---
+++ header.php
@@ -0,0 +1,22 @@
+<html>
+<head>
+	<script src="http://code.jquery.com/jquery-2.1.1.min.js"></script>
+	<script language="javascript" src="js/bootstrap.js"></script>
+	<link rel="stylesheet" href="css/bootstrap.min.css"/>
+</head>
+<body>
+	<div class="container">
+
+		<div class="row">
+		
+			<div class="col-md-8">
+				<h3>NEIL DANTAS</h3>
+			</div>
+			
+			<div class="col-md-4">
+			menu
+			</div>
+			
+		</div>
+		<hr/>
+			
---
+++ js/fullslider.js
@@ -0,0 +1,8 @@
+<script language="javascript">
+//Handles the carousel thumbnails
+       $('[id^=carousel-selector-]').click( function(){
+            var id = this.id.substr(this.id.lastIndexOf("-") + 1);
+            var id = parseInt(id);
+            $('#myCarousel').carousel(id);
+        });
+</script>
